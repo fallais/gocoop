2acf66db622ae9b9be22195b699f2d79d25d80f1
François ALLAIS <francois.allais@hotmail.com>
2020-01-19 10:54:41 +0100

    split windows and linux

---
+++ pkg/coop/door/door_windows.go
@@ -0,0 +1,41 @@
+// +build windows
+package door
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+//------------------------------------------------------------------------------
+// Structure
+//------------------------------------------------------------------------------
+
+// Door is a physical door manipulated with a motor.
+type Door struct {
+}
+
+//------------------------------------------------------------------------------
+// Factory
+//------------------------------------------------------------------------------
+
+// NewDoor returns a new Door.
+func NewDoor(openingDuration, closingDuration time.Duration) *Door {
+	return &Door{}
+}
+
+//------------------------------------------------------------------------------
+// Functions
+//------------------------------------------------------------------------------
+
+// Open the door
+func (d *Door) Open() error {
+	logrus.Warningln("Coop cannot work on Windows")
+	return nil
+}
+
+// Close the door
+func (d *Door) Close() error {
+	logrus.Warningln("Coop cannot work on Windows")
+	return nil
+}
